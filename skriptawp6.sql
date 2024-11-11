a419194bf4efab53f15aedb8c55ce54151cf0524
janustanja <janustanja@gmail.com>
2024-11-11 18:42:28 +0100

    Kreiranje tablica grupe, polaznici, clanovi

--- skriptawp6.sql
+++ skriptawp6.sql
@@ -5,10 +5,32 @@
 
 --use edunovawp6;
 
-create table smjerovi(
-sifra int,
-naziv varchar(50),
-cijena decimal(18,2),
-izvodiseod datetime,
-vaucer bit
+--create table smjerovi(
+--sifra int,
+--naziv varchar(50),
+--cijena decimal(18,2),
+--izvodiseod datetime,
+--vaucer bit
+--);
+
+--create table grupe(
+--sifra int,
+--naziv varchar(20),
+--velicinagrupe int,
+--predavac varchar(50),
+--smjer int
+--);
+--use edunovawp6;
+
+--create table polaznici(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--email varchar(100),
+--oib char(11)
+--);
+
+create table clanovi(
+grupa int,
+polaznik int
 );
