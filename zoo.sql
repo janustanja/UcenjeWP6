cee384a4b6a79abf35c824b99c3c0e07e493a70c
janustanja <janustanja@gmail.com>
2024-11-13 17:59:18 +0100

    Kreiranje tabli prostorija, djelatnik, datum

--- zoo.sql
+++ zoo.sql
@@ -1,5 +1,15 @@
---create database zoo;
---use zoo;
+
+use master;
+go 
+--brisem postojecu bazu ako postoji
+drop database if exists zoo;
+go
+--kreiram novu bazu
+create database zoo; 
+go
+--prebacujem se na novu bazu
+use zoo;
+go
 
 create table zivotinja(
 sifra int,
@@ -8,4 +18,25 @@ ime varchar(50),
 djelatnik int,
 prostorija int,
 datum int
+);
+ 
+create table prostorija(
+sifra int,
+dimenzije varchar(30),
+maks_broj int,
+mjesto varchar(30)
+);
+
+create table djelatnik(
+sifra int,
+ime varchar(50),
+prezime varchar(50),
+iban varchar(50)
+);
+
+create table datum(
+d_rodenja datetime,
+d_dolaska datetime,
+d_smrti datetime,
+sifra int
 );
