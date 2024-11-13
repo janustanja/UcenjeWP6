ec99467cc71c0e3eae00937839cf4b917091c59e
janustanja <janustanja@gmail.com>
2024-11-13 18:16:54 +0100

    Povezivanje veza izmedu tablica

--- zoo.sql
+++ zoo.sql
@@ -11,32 +11,33 @@ go
 use zoo;
 go
 
-create table zivotinja(
-sifra int,
-vrsta varchar(50),
-ime varchar(50),
-djelatnik int,
-prostorija int,
-datum int
-);
- 
+
 create table prostorija(
-sifra int,
-dimenzije varchar(30),
-maks_broj int,
-mjesto varchar(30)
+sifra int not null primary key identity(1,1),
+dimenzije varchar(30) not null,
+maks_broj int not null,
+mjesto varchar(30) not null
 );
 
 create table djelatnik(
-sifra int,
-ime varchar(50),
-prezime varchar(50),
-iban varchar(50)
+sifra int not null primary key identity(1,1) ,
+ime varchar(50) not null,
+prezime varchar(50) not null,
+iban varchar(50) not null
 );
 
 create table datum(
-d_rodenja datetime,
-d_dolaska datetime,
-d_smrti datetime,
-sifra int
+sifra int not null primary key identity(1,1),
+d_rodenja datetime not null,
+d_dolaska datetime not null,
+d_smrti datetime not null
+);
+
+create table zivotinja(
+sifra int not null primary key identity(1,1) ,
+vrsta varchar(50) not null,
+ime varchar(50) not null,
+djelatnik int not null references djelatnik(sifra),
+prostorija int not null references prostorija(sifra),
+datum int not null references datum(sifra)
 );
