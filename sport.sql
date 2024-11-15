b447e9616bb2175f28a988f33fbdac618d9eac27
tanjanus <102169254+tanjanus@users.noreply.github.com>
2024-11-15 14:36:53 +0100

    Dovrsen zadatak

--- sport.sql
+++ sport.sql
@@ -1,16 +1,16 @@
 use master;
 go 
 
-drop database if exists sport;
+drop database if exists nogomet;
 go
 
-create database sport; 
+create database nogomet; 
 go
 
-use sport;
+use nogomet;
 go
 
-create table klubovi(
+create table klub(
 sifra int not null primary key identity(1,1),
 naziv varchar(30) not null,
 osnovan datetime,
@@ -20,31 +20,31 @@ drzava varchar(30),
 liga int
 );
 
-create table utakmice(
+create table utakmica(
 sifra int not null primary key identity(1,1),
 datum datetime not null,
 vrijeme datetime not null,
 lokacija varchar(30) not null,
 stadion varchar(30) not null,
-domaci_klub int not null references klubovi(sifra),
-gostujuci_klub int not null references klubovi(sifra)
+domaci_klub int not null references klub(sifra),
+gostujuci_klub int not null references klub(sifra)
 );
 
-create table igraci(
+create table igrac(
 sifra int not null primary key identity(1,1),
 ime varchar(30) not null,
 prezime varchar(30) not null,
 datum_rodenja datetime not null,
 pozicija varchar(30) not null,
 broj_dresa int not null,
-klub int not null references klubovi(sifra)
+klub int not null references klub(sifra)
 );
 
-create table treneri(
+create table trener(
 sifra int not null primary key identity(1,1),
 ime varchar(30) not null,
 prezime varchar(30) not null,
-klub int not null references klubovi(sifra),
+klub int not null references klub(sifra),
 nacionalnost varchar(30) not null,
 iskustvo bit not null
 );
