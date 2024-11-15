0d0a0178c5cb61aafce2c94d147453882d3093e0
janustanja <janustanja@gmail.com>
2024-11-15 18:16:23 +0100

    Zavrsena baza

--- skriptawp6.sql
+++ skriptawp6.sql
@@ -95,3 +95,8 @@ INSERT INTO polaznici (ime, prezime, email) VALUES
 ('Luka', 'Kordić', 'kordic234@gmail.com'),
 ('Ivan', 'Režan', 'forexivanrezan@gmail.com'),
 ('Antonio', 'Simpf', 'antonijosimpf@gmail.com');
+
+insert into clanovi (grupa, polaznik)
+values (1,1), (1,2), (1,3), (1,4), (1,5), (1,6), (1,7), (1,8),
+(1,9), (1,10), (1,11), (1,12), (1,13), (1,14), (1,15), (1,16), 
+(1,17), (1,18), (1,19), (1,20), (1,21), (1,22), (1,23), (1,24);
