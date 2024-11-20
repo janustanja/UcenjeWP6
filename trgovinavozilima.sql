c3784ad546f16fe10252bd4e2a0ec34f09e7cc30
janustanja <janustanja@gmail.com>
2024-11-20 11:16:12 +0100

    Unos u tablice vrstavozila, kupac, dobavljac i vozilo

--- trgovinavozilima.sql
+++ trgovinavozilima.sql
@@ -1,4 +1,4 @@
-use master;
+﻿use master;
 go
 
 drop database if exists trgovina;
@@ -19,7 +19,7 @@ sifra int not null primary key identity(1,1),
 ime varchar(30) not null,
 prezime varchar(30) not null,
 adresa varchar(100) not null,
-iban varchar(30) 
+iban varchar(30) not null
 );
 
 create table dobavljac (
@@ -39,5 +39,31 @@ prijedenikm int not null,
 cijena decimal(18,2) not null,
 kupac int not null references kupac(sifra)
 );
+insert into vrstavozila (naziv)
+values 
+('Automobil'),
+('Autobus'),
+('Kombi'),
+('Kamion');
 
+insert into kupac (ime, prezime, adresa, iban)
+values
+('Mirko', 'Marić', 'Matije Gupca 55 Virovitica', 'HR8452254785126447529'),
+('Ivana', 'Kovač', 'Trg bana Josipa Jelačića 2 Zagreb', 'HR8544120326558945258'),
+('Jakov', 'Ban', 'Svačićev trg 23 Osijek', 'HR8452654854215978854'),
+('Dijana', 'Dombić', 'Miroslava Krleže 75 Varaždin', 'HR7412589632145698753');
+
+insert into dobavljac (naziv, adresa, iban)
+values
+('Transport Franić', 'Boškovićeva 13 Beli Manastir', 'HR4552166903487526781'),
+('Dominković Prijevoz', 'Cvjetni trg 1 Zagreb', 'HR6223014577521000326'),
+('Auto kuća Maras', 'Dalmatinska ulica 8 Split', 'HR1254865770126895587');
+
+insert into vozilo (vrstavozila, dobavljac, marka, godproizvodnje, prijedenikm, cijena, kupac)
+values
+(1, 3, 'Volvo', '2014', 122000, 11000, 4 ),
+(4, 1, 'Man', '2008', 682000, 21000, 1 ),
+(2, 2, 'Setra', '2000', 800000, 50000, 3 ),
+(3, 3, 'Mercedes', '2020', 50000, 33000, 3 )
+;
 
