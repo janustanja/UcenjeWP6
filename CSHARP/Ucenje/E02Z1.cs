5c95cb732fb4fd4520f11cc56165fdca6cae0e70
janustanja <janustanja@gmail.com>
2024-12-04 17:59:03 +0100

    E02Z1 zadatak

---
+++ CSHARP/Ucenje/E02Z1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E02Z1
+    {
+
+        public static void Izvedi()
+        {
+           
+
+
+            Console.Write("Unesi ime: ");
+            string ime = Console.ReadLine();
+
+            int i;
+            Console.WriteLine("Upiši broj godina: ");
+            i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Osoba {0} ima {1} godina.", ime, i);
+
+
+        }
+
+    }
+}
