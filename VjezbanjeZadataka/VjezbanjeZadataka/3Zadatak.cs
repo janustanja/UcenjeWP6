1912b23385ad62ad92e43e8e314773b7a4fa45fe
janustanja <janustanja@gmail.com>
2024-12-07 11:00:04 +0100

    3Zadatak

---
+++ VjezbanjeZadataka/VjezbanjeZadataka/3Zadatak.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VjezbanjeZadataka
+{
+    internal class _3Zadatak
+    {
+        public static void Izvedi()
+        {
+            Console.WriteLine("Unesi cijeli broj:");
+            int broj = int.Parse(Console.ReadLine());
+            if (broj % 2 == 0)
+            {
+                Console.WriteLine("Paran.");
+            }
+            else
+            {
+                Console.WriteLine("Neparan.");
+            }
+            Console.WriteLine(broj % 2 == 0 ? "Paran!" : "Neparan!");    //kraci nacin
+        }
+    }
+}
