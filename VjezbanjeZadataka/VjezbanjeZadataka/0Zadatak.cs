dec67656297e94db332de4f2be72e3b783288e5c
janustanja <janustanja@gmail.com>
2024-12-07 10:59:54 +0100

    0Zadatak

---
+++ VjezbanjeZadataka/VjezbanjeZadataka/0Zadatak.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VjezbanjeZadataka
+{
+    internal class _0Zadatak
+    {
+        public static void Izvedi()
+        {
+            Console.WriteLine("Unesite svoje ime:");
+            string ime = Console.ReadLine();
+            Console.WriteLine("Unesite broj godina:");
+            int godine = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Osoba {0} ima {1} godina!", ime, godine);
+        }
+    }
+}
--- VjezbanjeZadataka/VjezbanjeZadataka/Program.cs
+++ VjezbanjeZadataka/VjezbanjeZadataka/Program.cs
@@ -1,5 +1,8 @@
 ﻿
 
 using VjezbanjeZadataka;
+_0Zadatak.Izvedi();
 //_1Zadatak.Izvedi();
-_Zadatak2.Izvedi();
+//_Zadatak2.Izvedi();
+//_3Zadatak.Izvedi();
+//_4Zadatak.Izvedi();
