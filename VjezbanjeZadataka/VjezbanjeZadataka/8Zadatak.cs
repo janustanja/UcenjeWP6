d48ecc9d9247962df7f280af8277ddc8176a1d6f
janustanja <janustanja@gmail.com>
2024-12-11 18:18:19 +0100

    8Zadatak

---
+++ VjezbanjeZadataka/VjezbanjeZadataka/8Zadatak.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VjezbanjeZadataka
+{
+    internal class _8Zadatak
+    {
+        public static void Izvedi()
+        {
+            //Console.WriteLine("E09");
+            int broj, suma = 0;
+            while (true)
+            {
+                Console.WriteLine("Unosi brojeve (kada je kraj unesi -1): ");
+                broj = int.Parse(Console.ReadLine());
+                if (broj == -1)
+                {
+                    break;
+                }
+                suma = suma + broj;
+            }
+            Console.WriteLine(suma);
+        }
+    }
+}
+
