81609f10e56f98b51921bfd590264634fb4bfc55
janustanja <janustanja@gmail.com>
2024-12-11 14:04:54 +0100

    6Zadatak

---
+++ VjezbanjeZadataka/VjezbanjeZadataka/6Zadatak.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VjezbanjeZadataka
+{
+    internal class _6Zadatak
+    {
+        public static void Izvedi()
+        {
+            Console.WriteLine("Unesite 1. cijeli broj: ");
+            int broj1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Unesite 2. cijeli broj: ");
+            int broj2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Brojevi između {0} i {1} su: ", broj1, broj2);
+
+            if (broj1 < broj2)
+            {
+                for (int i = broj1; i <= broj2; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else if (broj1 > broj2)
+            {
+                for (int i = broj2; i >= broj1; i--)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Greška! Unesli ste iste brojeve.");
+            }
+
+
+
+
+            
+        }
+    }
+}
