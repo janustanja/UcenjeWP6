85b86202c054358f379fac8043efa5c470ac5cf1
janustanja <janustanja@gmail.com>
2024-12-11 18:54:24 +0100

    E10Z1

---
+++ CSHARP/Ucenje/E10Z1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E10Z1
+    {
+        public static void Izvedi()
+        {
+            //Console.WriteLine("E10");
+
+            Console.Write("Unesi izraz (bez razmaka): ");
+            string izraz= Console.ReadLine();
+            if (izraz.Length % 2 == 0)
+            {
+                //Console.WriteLine("Parno");
+                bool palindrom = true;
+                for (int i = 0; i< izraz.Length /2; i++)
+                {
+                    if (izraz[i] != izraz[izraz.Length - 1 - i])
+                    {
+                        palindrom = false;
+                        break;
+                    }
+                }
+                Console.WriteLine(palindrom ? "Da" : "Ne");
+            }
+            else
+            {
+                Console.WriteLine("Neparno");
+
+            }
+
+            
+        }
+    }
+}
